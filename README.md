53534ae38570b6f3146d6017d4e3e90247dac136
Bandi017 <bandicode017@gmail.com>
2022-10-01 17:54:14 +0200

    create mp3 from mp4 working

--- README.md
+++ README.md
@@ -4,9 +4,15 @@ PreRequisites:
 - moviepy   # https://zulko.github.io/moviepy/index.html
 - shutil 
 - glob 
+Operating systems:
+Ubuntu - ok 
+
 
 TODO:
 1. use type hinting for prompts
 2. add possibilty to search for title
 3. testing 
 4. Interface ?
+5. adding a loader - terminal version ?
+6. exit option at each step
+7. refactor code
--- get_yt_vid.py
+++ get_yt_vid.py
@@ -4,7 +4,7 @@ import shutil
 import glob
 
 from os import path
-from pytube import YouTube    # https://pytube.io/en/latest/
+from pytube import YouTube  # https://pytube.io/en/latest/
 from moviepy.editor import *  # https://zulko.github.io/moviepy/index.html
 
 video_id = input("Enter video is from url: ")
@@ -48,11 +48,11 @@ def change_mp4_location(prompt):
         except ValueError:
             print("Sorry, wrong input")
             continue
-
+        get_mp4_file = glob.glob('./*.mp4')[0]
         if move_file == "yes" or move_file == 'y':
             check_videos_folder('videos')
             print("moving file")
-            get_mp4_file = glob.glob('./*.mp4')[0]
+
             try:
                 orig_file = get_mp4_file
                 dest_file = f"./videos/{orig_file[2:]}"
@@ -63,15 +63,15 @@ def change_mp4_location(prompt):
             except DoesNotExist as e:
                 print(e)
         elif move_file == "no" or move_file == "n":
-            print(get_mp4_file)
-            #ask_for_mp3("convert to MP3?", get_mp4_file)
-            print("all done!!!")
+            mp4_file = get_mp4_file[2:]
+            print(mp4_file)
+            ask_for_mp3("convert to MP3?(yes/no)", get_mp4_file)
             break
         else:
             print("use only yes(y) or no(n): ")
 
 
-def ask_for_mp3(mp3_prompt, mp4_file):
+def ask_for_mp3(mp3_prompt, file):
     while True:
         try:
             get_answer = input(mp3_prompt).lower()
@@ -80,24 +80,25 @@ def ask_for_mp3(mp3_prompt, mp4_file):
             continue
 
         if get_answer == 'yes' or get_answer == 'y':
-            convert_to_mp3(mp4_file)
+            convert_to_mp3(file)
+            break
         elif get_answer == 'no' or get_answer == 'n':
             print("all done!!!")
             break
         else:
             print("use only yes(y) or no(n): ")
 
+
 def convert_to_mp3(mp4_file):
-    mp4_file_name = mp4_file[0]
-    mp3_file_name = mp4_file_name[2:-3] + "mp3"
-    clip = VideoFileClip(mp4_file_name)
+    mp3_file_name = mp4_file[2:-3] + "mp3"
+    clip = VideoFileClip(mp4_file)
     clip.audio.write_audiofile(mp3_file_name)
     print("created mp3 file and deleting original file")
     os.remove(mp4_file)
+
 
 # hYh_RkkgFd0 test-video-id
 
 
 get_you_tube_file(video_id)
-
 change_mp4_location("move to videos folder? (y/n)")
